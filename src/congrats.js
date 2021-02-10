bc1e1a1a6e708445fa462ef5ef4a062bc7b04ccb
Dauda Hammed Oluwatoyin <dauda.hammed132@gmail.com>
2021-02-10 14:39:43 +0100

    add propTypes checking to congrats components

---
+++ src/congrats.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+
+/**
+ * Functional react component for congratulatory message.
+ * @function
+ * @returns {JSX.Element} -Rendered component (or null if `success` prop is )
+ */
+
+ const Congrats = (props) => {
+
+    if (props.success) {
+        return (
+            <div>
+                <div data-test="component-congrats">
+                <span data-test="congrats-message">
+                Congratulations! you guessed the word!
+                </span>
+                </div>
+            </div>
+        );} else {
+
+            return (
+                <div data-test ="component-congrats" />
+            );
+        }
+    
+   
+ }
+
+ Congrats.propTypes = {
+     success: PropTypes.bool.isRequired,
+ }
+
+ export default Congrats;
